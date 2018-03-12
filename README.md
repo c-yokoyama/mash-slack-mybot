4784c947b81ed85cda4892d2ba5be1a528ff21f2
Chihiro Yokoyama <c.yokoyama.ttr@gmail.com>
2018-03-12 23:58:45 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# mash-slack-mybot
+
+## Getting Started
+
+```
+source credentials.sh
+```
+
+---
+
+## ref
+
+https://developer.health.nokia.com/api?_ga=2.42632934.2129126591.1518093763-725386839.1518093763
+https://github.com/jrmycanady/nokiahealth
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	//"mash-slack-mybot/nokiahealth"
+	"github.com/nlopes/slack"
+)
+
+func getMyMeasures() string {
+	return "your-measures-value"
+}
+
+func main() {
+	api := slack.New(os.Getenv("TOKEN"))
+	botUserID := os.Getenv("BOTUSER_ID")
+
+	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
+	slack.SetLogger(logger)
+	api.SetDebug(true)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		fmt.Print("EventReceived: ")
+		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			// Ignore hello
+
+		case *slack.ConnectedEvent:
+			//fmt.Println("Infos:", ev.Info)
+			//fmt.Println("Connection counter:", ev.ConnectionCount)
+			//Replace C2147483705 with your Channel ID
+			//rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", "C2147483705"))
+
+		case *slack.MessageEvent:
+			fmt.Printf("Message: %v\n", ev)
+
+			// Msg includes mention to botUser
+			if strings.HasPrefix(ev.Msg.Text, "<@"+botUserID+">") {
+				args := strings.Split(ev.Msg.Text, " ")
+				if len(args) <= 1 {
+					break
+				}
+				switch args[1] {
+				case "measure":
+					fmt.Printf("Measure: ")
+					fmt.Println(getMyMeasures())
+				}
+			}
+		case *slack.PresenceChangeEvent:
+			//fmt.Printf("Presence Change: %v\n", ev)
+
+		case *slack.LatencyReport:
+			//fmt.Printf("Current latency: %v\n", ev.Value)
+
+		case *slack.RTMError:
+			fmt.Printf("Error: %s\n", ev.Error())
+
+		case *slack.InvalidAuthEvent:
+			fmt.Printf("Invalid credentials")
+			return
+
+		default:
+			// fmt.Printf("Unexpected: %v\n", msg.Data)
+		}
+	}
+}
